d0bd0b41e2592e58e4d627663f2d75b8b0540e45
KreloX <31541291+KreloX@users.noreply.github.com>
2022-03-18 15:49:36 +0100

    Version 1.1.1

--- src/main/java/krelox/metallurgychests/util/Reference.java
+++ src/main/java/krelox/metallurgychests/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MODID = "metallurgychests";
 	public static final String NAME = "Metallurgy Chests";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	public static final String DEPENDENCIES = "required-after:metallurgy";
 
 	public static final String CLIENT = "krelox.metallurgychests.util.ClientProxy";
